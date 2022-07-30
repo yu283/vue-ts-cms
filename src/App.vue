0d4f3c2d082530f25b830869bd75c734ecfbd864
Renhy <283444701@qq.com>
2022-07-30 20:34:33 +0800

    style(main): main rhy style

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>App</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+})
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
